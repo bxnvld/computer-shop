6cc75c1cf351e84f3ef69c66c90db1fd0b5bcfbe
bxnvld <majstrenkoandrian@gmail.com>
2024-05-22 20:57:25 +0300

    Adding real date for admin in account

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -50,8 +50,22 @@ exports.getSignForm = catchAsync(async (req, res) => {
 });
 
 exports.getAccount = (req, res) => {
+  const timestamp = Date.now();
+  const dateObj = new Date(timestamp);
+  const month   = dateObj.getUTCMonth() + 1; // months from 1-12
+  const day     = dateObj.getUTCDate();
+  const year    = dateObj.getUTCFullYear();
+
+  // Using padded values, so that 2023/1/7 becomes 2023/01/07
+  const pMonth        = month.toString().padStart(2,"0");
+  const pDay          = day.toString().padStart(2,"0");
+  const newPaddedDate = `${year}/${pMonth}/${pDay}`;
+
   res.status(200).render("account", {
     title: "Your account",
+    year,
+    pMonth,
+    pDay
   });
 };
 
